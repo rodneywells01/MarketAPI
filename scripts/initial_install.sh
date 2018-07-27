365fec0d1729dbae9c2ac67def37011557cb6840
Rodney Wells <rodneywells01@gmail.com>
2018-07-26 23:26:58 -0400

    removed sudo from init install

--- scripts/initial_install.sh
+++ scripts/initial_install.sh
@@ -5,7 +5,7 @@ pip install -e .
 
 
 # Install Mongodb - https://docs.mongodb.com/manual/tutorial/install-mongodb-enterprise-on-ubuntu/
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
 echo "deb [ arch=amd64,arm64,ppc64el,s390x ] http://repo.mongodb.com/apt/ubuntu xenial/mongodb-enterprise/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-enterprise.list
-sudo apt-get update # <-- This will take a while to execute
-sudo apt-get install -y mongodb-enterprise
+apt-get update # <-- This will take a while to execute
+apt-get install -y mongodb-enterprise
