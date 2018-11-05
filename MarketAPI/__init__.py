dd73d167353701fc209f5bc1a37b80e1cfa270c5
Rodney Wells <rodneywells@Rodneys-MacBook-Pro.local>
2018-11-04 19:20:23 -0500

    Testing Refactor; Dev Requirements

--- MarketAPI/__init__.py
+++ MarketAPI/__init__.py
@@ -12,21 +12,35 @@ import json
 from bson import ObjectId
 
 class JSONEncoder(json.JSONEncoder):
-    def default(self, o):
-        if isinstance(o, ObjectId):
-            return str(o)
-        return json.JSONEncoder.default(self, o)
+	def default(self, o):
+		if isinstance(o, ObjectId):
+			return str(o)
+		return json.JSONEncoder.default(self, o)
+
+
+def connect_db(app): 
+	# Configure DB
+	app.config["MONGO_URI"] = "mongodb://localhost:27017/WatchList"
+	return PyMongo(app)
+
+	# Debug stuff 
+	print(type(app.mongo))
+	print(type(app.mongo.db))
+	print(type(app.mongo.cx))
+
 
 def create_app(config):
+	# TESTing = True, Database = DB PAth 
 	app = Flask(__name__)
-	app.config["MONGO_URI"] = "mongodb://localhost:27017/WatchList"
+
+	if app.config["TESTING"]: 
+		# Spin up a DB
+		pass 
+
 	app.register_blueprint(healthcheck)
 	app.register_blueprint(users)
 	app.json_encoder = JSONEncoder
 
-	# Configure DB
-	app.mongo = PyMongo(app)
+	app.mongo = connect_db(app)
 
 	return app
-
-
--- tests/test_health.py
+++ tests/test_health.py
@@ -1,30 +1,36 @@
 import os
 import tempfile
-
 import pytest
+from mock import Mock
 
-from MarketAPI import create_app
+from MarketAPI import create_app, connect_db
 
 # https://github.com/pallets/flask/blob/1.0.2/examples/tutorial/tests/conftest.py
 @pytest.fixture
-def app():
-    """Create and configure a new app instance for each test."""
-    # create a temporary file to isolate the database for each test
-    db_fd, db_path = tempfile.mkstemp()
+def client():
+	"""
+	Create and configure a new app instance for each test.
+	"""
+	
+	# create a temporary file to isolate the database for each test
+	db_fd, db_path = tempfile.mkstemp()
 
-    # create the app with common test config
-    app = create_app({
-        'TESTING': True,
-        'DATABASE': db_path,
-    })
+	connect_db = Mock(return_value=None)
 
-    # create the database and load test data
-    with app.app_context():
-        init_db()
-        get_db().executescript(_data_sql)
+	# create the app with common test config
+	app = create_app({
+		'TESTING': True,
+		'DATABASE': db_path,
+	})
 
-    yield app
+	yield app.test_client()
 
-    # close and remove the temporary database
-    os.close(db_fd)
-    os.unlink(db_path)
+	# close and remove the temporary database
+	os.close(db_fd)
+	os.unlink(db_path)
+
+def test_healthcheck(client): 
+	response = client.get("/health")
+	print("Hello")
+	assert response.status_code == 200
+	
