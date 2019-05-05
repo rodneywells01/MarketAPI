cf13c3f2c1dcf8097d93f639e0a0f4d9c491a0a9
Rodney Wells <rodneywells01@gmail.com>
2019-05-05 11:06:36 -0400

    Minor test fixes

---
+++ tests/test_users.py
@@ -0,0 +1,49 @@
+import os
+import tempfile
+import pytest
+from mock import Mock
+
+from MarketAPI import create_app, connect_db
+
+
+@pytest.fixture
+def client():
+	"""
+	Create and configure a new app instance for each test.
+	"""
+	
+	# create a temporary file to isolate the database for each test
+	db_fd, db_path = tempfile.mkstemp()
+
+	connect_db = Mock(return_value=None)
+
+	# create the app with common test config
+	app = create_app({
+		'TESTING': True,
+		'DATABASE': db_path,
+	})
+
+
+	# Mocked DB Responses
+	app.mongo = Mock(
+		db = Mock(
+			users = Mock(
+				find_one = Mock(return_value={
+					"_id": "123",
+				    "age": 23,
+					"name": "Rodney Wells"
+				})
+			)
+		)
+	)
+
+	yield app.test_client()
+
+	# close and remove the temporary database
+	os.close(db_fd)
+	os.unlink(db_path)
+
+
+def test_get_user(client):
+	result = client.get("/test")
+	print(result.data)
